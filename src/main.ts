d1b1bb8eb35b360c4bd29fdab8e818408cf1bf2f
guimendel <guimendeln@gmail.com>
2023-05-03 20:24:09 -0300

    🔨Configure vuetify & scss

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,30 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
-import './assets/main.css'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import './assets/main.scss'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: { fa }
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
