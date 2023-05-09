114b350f4887bbf66d7bd707e808582ce596c941
guimendel <guimendeln@gmail.com>
2023-05-09 19:50:19 -0300

    🔨Landing page

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,9 +1,25 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import Keycloak from 'keycloak-js'
 
 export const useAuthStore = defineStore('auth', () => {
   // Whether user is authenticated
   const isAuthenticated = ref(false)
+
+  // Create keycloak client instance
+  const client = new Keycloak({
+    url: import.meta.env.VITE_KEYCLOAK_URL,
+    realm: import.meta.env.VITE_KEYCLOAK_REALM,
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT
+  })
+
+  client
+    // .init({})
+    .init({ onLoad: 'check-sso' })
+    .then((authenticated) => (isAuthenticated.value = authenticated))
+    .catch((reason) => {
+      if (reason) console.error(reason)
+    })
 
   return { isAuthenticated }
 })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,23 @@
 <template>
-  <p>Please log in!</p>
+  <div class="d-flex h-100 w-100 align-center">
+    <!-- Random image cover -->
+    <div class="d-flex h-100" style="flex: 1.5">
+      <VImg :src="`https://picsum.photos/1920/1080`" cover />
+    </div>
+
+    <!-- Start section -->
+    <VCard class="d-flex flex-column h-100 justify-center pa-10 h-100" style="flex: 1; gap: 2rem">
+      <VCardTitle class="text-h3 text-wrap font-weight-bold" style="line-height: 1em">Risk Assesser Demo</VCardTitle>
+      <VCardSubtitle class="text-h6 text-wrap" style="line-height: 1.2em"
+        >Please sign in via keycloak to start</VCardSubtitle
+      >
+
+      <VCardActions>
+        <VBtn class="px-5" color="blue" prepend-icon="fas fa-sign-in-alt">Sign In</VBtn>
+      </VCardActions>
+    </VCard>
+    <!-- <div class="d-flex h-100 align-center justify-center" style="flex: 1; box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.4); z-index: 10;">
+      <VCard style="box-shadow: none;" title="Risk Assesser Demo" />
+    </div> -->
+  </div>
 </template>
